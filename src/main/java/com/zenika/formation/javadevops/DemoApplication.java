a80dfde242cd7397aabef4b2d6c855bf96ac717a
clevandowski <cyrille.levandowski@zenika.com>
2017-06-09 16:56:59 +0200

    Affiche adresse:port du host

--- src/main/java/com/zenika/formation/javadevops/DemoApplication.java
+++ src/main/java/com/zenika/formation/javadevops/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.zenika.formation.javadevops;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,13 +14,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class DemoApplication {
-  @RequestMapping("/")
-  @ResponseBody
-  String home() {
-    return "Hello World !!!";
-  }
 
-  public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
-  }
+	@Autowired
+	Environment environment;
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		try {
+      return "Hello World !!! on " + InetAddress.getLocalHost().getHostAddress() + ":" + environment.getProperty("local.server.port");
+    } catch (UnknownHostException e) {
+      throw new RuntimeException("Error when trying to get local IP address", e);
+    }
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 }
