cea66144eef4c9c4b0de9114c992233670d049a5
clevandowski <cyrille.levandowski@zenika.com>
2017-06-09 14:16:18 +0200

    Changement du request mapping

--- src/main/java/com/zenika/formation/javadevops/DemoApplication.java
+++ src/main/java/com/zenika/formation/javadevops/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class DemoApplication {
-  @RequestMapping("/")
+  @RequestMapping("/hello")
   @ResponseBody
   String home() {
     return "Hello World !!!";
